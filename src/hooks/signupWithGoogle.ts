69bf84cfeec4bc7a816b22905eae6a47ea894612
sanchris1 <samchris12737@gmail.com>
2026-08-20 00:27:52 +0300

    updated some env config

--- src/hooks/signupWithGoogle.ts
+++ src/hooks/signupWithGoogle.ts
@@ -1,7 +1,7 @@
 import { authClient } from "@/lib/auth-client";
 
 const callbackURL =
-  process.env.NODE_ENV === "production"
+  process.env.NODE_ENV === "development"
     ? "http://localhost:3000"
     : "https://trails-and-memoirs.vercel.app";
 
