93ddd1176accce55fa1197fb29c4b2d61559cef7
sanchris1 <samchris12737@gmail.com>
2026-08-30 17:29:02 +0300

    changed the frontend url to enable getting the session

--- src/hooks/getSession.ts
+++ src/hooks/getSession.ts
@@ -4,17 +4,14 @@ export async function getSession() {
   const h = await headers();
   const rawCookie = h.get("cookie") || "";
 
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/auth/get-session`,
-    {
-      method: "GET",
-      headers: {
-        cookie: rawCookie,
-        Accept: "application/json",
-      },
-      cache: "no-store",
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/me`, {
+    method: "GET",
+    headers: {
+      cookie: rawCookie,
+      Accept: "application/json",
     },
-  );
+    cache: "no-store",
+  });
 
   if (!res.ok) return null;
   return res.json();
