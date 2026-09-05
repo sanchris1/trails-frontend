dd2381f1d1ed8b98ae63ac223e44e73f83661f8d
sanchris1 <samchris12737@gmail.com>
2026-09-05 21:25:31 +0300

    using query client to invalidate the session

--- src/hooks/auth/useLogoutUser.ts
+++ src/hooks/auth/useLogoutUser.ts
@@ -1,16 +1,17 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { logoutUser } from "../logoutUser";
 import { tokenStore } from "@/lib/tokenStore";
 import toast from "react-hot-toast";
 
 export const useLogoutUser = () => {
-  // const queryClient = useQueryClient();
+  const queryClient = useQueryClient();
 
   return useMutation({
     mutationFn: logoutUser,
 
     onSuccess: () => {
       tokenStore.clear();
+      queryClient.setQueryData(["session"], null);
     },
     onError: () => {
       toast.error("Error occurred while logging out.");
